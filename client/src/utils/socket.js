061b661291d8b31eccb5c1a1530f20622bb2c99c
Gajendra <gajendranh1092@gmail.com>
2026-04-19 12:09:05 +0530

    fix(ws): force websocket transport for cluster stability

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -2,5 +2,6 @@ import { io } from 'socket.io-client';
 
 const URL = process.env.REACT_APP_SERVER_URL;
 export const socket = io(URL, {
-  autoConnect: false
+  autoConnect: false,
+  transports: ['websocket'] // Force WebSockets only
 });
